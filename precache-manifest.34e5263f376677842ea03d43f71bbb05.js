f63bc6e63abcdea55a05ac12a7229d39374d258a
Aaron Lu <aaron_lu1@yahoo.com>
2019-02-03 14:36:26 +0800

    Updates

---
+++ precache-manifest.34e5263f376677842ea03d43f71bbb05.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3f718eee25f5f5b140b",
+    "url": "/remote-assignments/week-4/react-app/static/css/main.eee0575c.chunk.css"
+  },
+  {
+    "revision": "f3f718eee25f5f5b140b",
+    "url": "/remote-assignments/week-4/react-app/static/js/main.f3f718ee.chunk.js"
+  },
+  {
+    "revision": "629d0d5608d627e95316",
+    "url": "/remote-assignments/week-4/react-app/static/js/1.629d0d56.chunk.js"
+  },
+  {
+    "revision": "65e003282ba6a9eb5eb7",
+    "url": "/remote-assignments/week-4/react-app/static/js/runtime~main.65e00328.js"
+  },
+  {
+    "revision": "914b8563b5b4b992d0a1566cf8730933",
+    "url": "/remote-assignments/week-4/react-app/static/media/jay-wennington-2065-unsplash.914b8563.jpg"
+  },
+  {
+    "revision": "3ef389dd1ae4e911c933235cb8bbe162",
+    "url": "/remote-assignments/week-4/react-app/static/media/erik-odiin-539947-unsplash.3ef389dd.jpg"
+  },
+  {
+    "revision": "25aea88d7bd1ae652c6fdb9472304f22",
+    "url": "/remote-assignments/week-4/react-app/static/media/nikola-ancevski-531651-unsplash.25aea88d.jpg"
+  },
+  {
+    "revision": "03a3553015506f967964ee74d520d689",
+    "url": "/remote-assignments/week-4/react-app/static/media/john-matychuk-393455-unsplash.03a35530.jpg"
+  },
+  {
+    "revision": "1369fdae5c6f85849bdba6802a629435",
+    "url": "/remote-assignments/week-4/react-app/static/media/icons8-menu.1369fdae.svg"
+  },
+  {
+    "revision": "a66d16f987fc7d84b70e8965f31bc8a7",
+    "url": "/remote-assignments/week-4/react-app/static/media/icons8-delete.a66d16f9.svg"
+  },
+  {
+    "revision": "068049c5d159bed36fe59f4814fe1e8b",
+    "url": "/remote-assignments/week-4/react-app/static/media/josue-isai-ramos-figueroa-587073-unsplash.068049c5.jpg"
+  },
+  {
+    "revision": "be48b6566a24fd82d0d171ea29b91c67",
+    "url": "/remote-assignments/week-4/react-app/index.html"
+  }
+];
